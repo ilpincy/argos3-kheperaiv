31ae1159a09eff7a6017cff9d8647d9fe3dfbc39
jadhavan <ajayabalan@wpi.edu>
2017-12-03 18:43:02 -0500

    added battery entity

--- src/plugins/robots/kheperaiv/simulator/kheperaiv_entity.cpp
+++ src/plugins/robots/kheperaiv/simulator/kheperaiv_entity.cpp
@@ -16,6 +16,7 @@
 #include <argos3/plugins/simulator/entities/light_sensor_equipped_entity.h>
 #include <argos3/plugins/simulator/entities/proximity_sensor_equipped_entity.h>
 #include <argos3/plugins/simulator/entities/rab_equipped_entity.h>
+#include <argos3/plugins/simulator/entities/battery_equipped_entity.h>
 
 static CRadians ULTRASOUND_SENSOR_ANGLES[5] = {
    CRadians::ZERO,
@@ -40,7 +41,8 @@ namespace argos {
       m_pcProximitySensorEquippedEntity(NULL),
       m_pcUltrasoundSensorEquippedEntity(NULL),
       m_pcRABEquippedEntity(NULL),
-      m_pcWheeledEntity(NULL) {
+      m_pcWheeledEntity(NULL),
+      m_pcBatteryEquippedEntity(NULL) {
    }
 
    /****************************************/
@@ -51,7 +53,10 @@ namespace argos {
                                       const CVector3& c_position,
                                       const CQuaternion& c_orientation,
                                       Real f_rab_range,
-                                      size_t un_rab_data_size) :
+                                      size_t un_rab_data_size,
+                                      UInt16 un_bat_full_capacity,
+                                      Real f_bat_ideal_discharge,
+                                      Real f_bat_moving_discharge) :
       CComposableEntity(NULL, str_id),
       m_pcControllableEntity(NULL),
       m_pcEmbodiedEntity(NULL),
@@ -61,7 +66,8 @@ namespace argos {
       m_pcProximitySensorEquippedEntity(NULL),
       m_pcUltrasoundSensorEquippedEntity(NULL),
       m_pcRABEquippedEntity(NULL),
-      m_pcWheeledEntity(NULL) {
+      m_pcWheeledEntity(NULL),
+      m_pcBatteryEquippedEntity(NULL) {
       try {
          /*
           * Create and init components
@@ -156,6 +162,12 @@ namespace argos {
                                    *m_pcEmbodiedEntity,
                                    CVector3(0.0f, 0.0f, KHEPERAIV_BASE_TOP));
          AddComponent(*m_pcRABEquippedEntity);
+         /* Battery senesor equipped entity */
+         m_pcBatteryEquippedEntity = new CBatteryEquippedEntity(this,"battery",
+                                                                     un_bat_full_capacity,
+                                                                     f_bat_ideal_discharge,
+                                                                     f_bat_moving_discharge);
+         AddComponent(*m_pcBatteryEquippedEntity);
          /* Controllable entity
             It must be the last one, for actuators/sensors to link to composing entities correctly */
          m_pcControllableEntity = new CControllableEntity(this, "controller_0");
@@ -276,6 +288,18 @@ namespace argos {
                                    *m_pcEmbodiedEntity,
                                    CVector3(0.0f, 0.0f, KHEPERAIV_BASE_TOP));
          AddComponent(*m_pcRABEquippedEntity);
+         /* Battery sensor equipped entity */
+         UInt16 unFullCapacity = 3400;
+         GetNodeAttributeOrDefault(t_tree, "bat_full_capacity", unFullCapacity, unFullCapacity);
+         /* Parse discharge parameters */
+         Real fIdealDischarge = 0.0f;
+         Real fMovingDischarge= 0.0f;
+         GetNodeAttributeOrDefault(t_tree, "bat_ideal_discharge", fIdealDischarge, fIdealDischarge);
+         GetNodeAttributeOrDefault(t_tree, "bat_moving_discharge", fMovingDischarge, fMovingDischarge);
+
+         m_pcBatteryEquippedEntity = new CBatteryEquippedEntity(this,"battery",unFullCapacity,fIdealDischarge,fMovingDischarge);
+         AddComponent(*m_pcBatteryEquippedEntity);
+
          /* Controllable entity
             It must be the last one, for actuators/sensors to link to composing entities correctly */
          m_pcControllableEntity = new CControllableEntity(this);
@@ -314,6 +338,8 @@ namespace argos {
          m_pcLEDEquippedEntity->Update();
       if(m_pcRABEquippedEntity->IsEnabled())
          m_pcRABEquippedEntity->Update();
+      if(m_pcBatteryEquippedEntity->IsEnabled())
+          m_pcBatteryEquippedEntity->Update();
    }
 
    /****************************************/
